ea10a794eeb444c1f5f4d90da968ec2beb86c305
Martin Bickel <martin@asc-hq.org>
2000-08-19 16:25:53 +0000

    Added provisional mousecontrol.h , ndir.h

---
+++ source/mousecontrol.h
@@ -0,0 +1,10 @@
+class cmousecontrol {
+         protected:
+
+            int mousestat;
+         public:
+            cmousecontrol( void ) : mousestat ( 0 ) {};
+            void chkmouse ( void );
+            void reset ( void );
+};
+ 
---
+++ source/ndir.h
@@ -0,0 +1,8 @@
+#ifndef ndir_h
+ #define ndir_h
+
+ #include <direct.h> 
+ #define direct dirent
+ // typedef struct direct dirent;
+
+#endif
