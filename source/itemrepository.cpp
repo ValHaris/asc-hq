3df52b2d69296275d479b93956e5d3b1aa7bb34d
Martin Bickel <martin@asc-hq.org>
2001-10-17 10:23:33 +0000

    version now 1.9.4.1

--- source/itemrepository.cpp
+++ source/itemrepository.cpp
@@ -266,13 +266,15 @@ void  loadalltextfiles ( )
       c = ff.getnextname();
    }
    displayLogMessage ( 4, "loadalltextfiles completed\n");
+/*
    displayLogMessage ( 4, "Building inheritance...");
    for ( TextFileRepository::iterator i = textFileRepository.begin(); i != textFileRepository.end(); i++ ) {
       for ( TextPropertyList::iterator j = i->second.begin(); j != i->second.end(); j++ ) {
-         
+
       }
    }
    displayLogMessage ( 4, "done\n");
+*/   
 }
 
 
