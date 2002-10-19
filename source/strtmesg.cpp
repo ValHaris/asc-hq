9a0122711cc4d96cb3bdf5c6b95a5697544ebfc3
Martin Bickel <martin@asc-hq.org>
2002-10-19 18:46:54 +0000

    Updated version number to 1.11.0.2

--- source/strtmesg.cpp
+++ source/strtmesg.cpp
@@ -1,4 +1,4 @@
-//     $Id: strtmesg.cpp,v 1.127 2002-10-15 16:52:46 mbickel Exp $
+//     $Id: strtmesg.cpp,v 1.128 2002-10-19 18:46:54 mbickel Exp $
 
 /*
     This file is part of Advanced Strategic Command; http://www.asc-hq.de
@@ -22,7 +22,7 @@
 
 
 // These strings should be the same as the release tags in CVS !
-const char* asc_release="ASC1.11.0.1";
+const char* asc_release="ASC1.11.0.2";
 
 #include <stdio.h>
 #include "strtmesg.h"
