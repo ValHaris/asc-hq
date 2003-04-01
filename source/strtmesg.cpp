a0ea4e64a0332b483a1e9770359d737d30e0594c
Martin Bickel <martin@asc-hq.org>
2003-04-01 16:18:01 +0000

    *** empty log message ***

--- source/strtmesg.cpp
+++ source/strtmesg.cpp
@@ -1,4 +1,4 @@
-//     $Id: strtmesg.cpp,v 1.187 2003-03-31 20:29:16 mbickel Exp $
+//     $Id: strtmesg.cpp,v 1.188 2003-04-01 16:18:01 mbickel Exp $
 
 /*
     This file is part of Advanced Strategic Command; http://www.asc-hq.de
@@ -22,7 +22,7 @@
 
 
 // These strings should be the same as the release tags in CVS !
-const char* asc_release="ASC1.13.2.5";
+const char* asc_release="ASC1.13.2.6";
 
 #include <stdio.h>
 #include "strtmesg.h"
