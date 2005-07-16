682ab1e191be52c0dc084df8d0b0004565fa6423
Martin Bickel <martin@asc-hq.org>
2005-07-16 06:46:45 +0000

    Updated Copyright notice

--- source/strtmesg.cpp
+++ source/strtmesg.cpp
@@ -1,8 +1,8 @@
-//     $Id: strtmesg.cpp,v 1.304 2005-07-10 09:07:28 mbickel Exp $
+//     $Id: strtmesg.cpp,v 1.305 2005-07-16 06:46:45 mbickel Exp $
 
 /*
     This file is part of Advanced Strategic Command; http://www.asc-hq.de
-    Copyright (C) 1994-2003  Martin Bickel  and  Marc Schellenberger
+    Copyright (C) 1994-2005  Martin Bickel  and  Marc Schellenberger
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
