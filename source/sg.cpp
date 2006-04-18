2176549f9541299eab0e58638b318357e7b77ad0
Martin Bickel <martin@asc-hq.org>
2006-04-18 18:15:06 +0000

    Removed debug

--- source/sg.cpp
+++ source/sg.cpp
@@ -1126,7 +1126,7 @@ void deployMapPlayingHooks ( GameMap* map )
 
 int main(int argc, char *argv[] )
 {
-   setenv( "DISPLAY", "192.168.0.61:0", 1 );
+   // setenv( "DISPLAY", "192.168.0.61:0", 1 );
 
    assert ( sizeof(PointerSizedInt) == sizeof(int*));
 
