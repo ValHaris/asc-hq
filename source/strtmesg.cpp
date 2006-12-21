9c00b580ceed87ab6f461b02ff5b8da42fbeaf23
Martin Bickel <martin@asc-hq.org>
2006-12-21 14:11:26 +0000

    Update build and packaging system

--- source/strtmesg.cpp
+++ source/strtmesg.cpp
@@ -20,6 +20,7 @@
 
 
 // These strings should be the same as the release tags in CVS !
+// don't alter the format of this string, as quite a lot of tools evaluate it!
 const char* asc_release="ASC1.99.90.46";
 
 #include <stdio.h>
