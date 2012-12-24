49af7403b54631684b4ea28b0f47fcd4ff5acf04
Christian Schramm <christian.h.m.schramm@gmail.com>
2012-12-24 01:33:08 +0100

    minor: removed useless commented-out code

--- source/spfst.cpp
+++ source/spfst.cpp
@@ -494,11 +494,6 @@ void         calculateobject( MapField* fld,
 
    int c = 0;
    for ( int dir = 0; dir < sidenum; dir++) {
-      /*
-      int a = x + getnextdx( dir, y );
-      int b = y + getnextdy( dir);
-      MapField* fld2 = actmap->getField(a,b);
-      */
       MapField* fld2 = fld->neighboringFields[dir];
       if ( fld2 ) {
          if ( obj->netBehaviour & ObjectType::NetToSelf )
@@ -544,11 +539,6 @@ void         calculateobject( MapField* fld,
       int autoborder = 0;
       int count = 0;
       for ( int dir = 0; dir < sidenum; dir++) {
-         /*
-         int a = x + getnextdx( dir, y );
-         int b = y + getnextdy( dir);
-         MapField* fld2 = actmap->getField(a,b);
-         */
          MapField* fld2 = fld->neighboringFields[dir];
          if ( !fld2 ) {
             // if the field opposite of the border field is connected to, make a straight line out of the map.
