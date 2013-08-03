e0a7e7761cd38d7d70e8b583e624599169c93f1e
Martin Bickel <martin@asc-hq.org>
2013-08-03 19:40:12 +0200

    Updated version number

--- source/strtmesg.cpp
+++ source/strtmesg.cpp
@@ -21,7 +21,7 @@
 
 // These strings should be the same as the release tags in CVS !
 // don't alter the format of this string, as quite a lot of tools evaluate it!
-const char* asc_release="ASC2.5.107.130";
+const char* asc_release="ASC2.5.107.131";
 
 #include <stdio.h>
 #include "strtmesg.h"
